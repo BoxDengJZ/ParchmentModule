4171731529d1acbcc43749d41151cdfd932cbf8a
邓江洲 <dengjiangzhouReal@gmail.com>
2021-04-12 11:12:14 +0800

    done

---
+++ boom/Parchment/Parchment.h
@@ -0,0 +1,18 @@
+//
+//  Parchment.h
+//  Parchment
+//
+//  Created by Jz D on 2021/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Parchment.
+FOUNDATION_EXPORT double ParchmentVersionNumber;
+
+//! Project version string for Parchment.
+FOUNDATION_EXPORT const unsigned char ParchmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Parchment/PublicHeader.h>
+
+
--- boom/boom/ViewController.swift
+++ boom/boom/ViewController.swift
@@ -7,12 +7,23 @@
 
 import UIKit
 
+import Parchment
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor.white
+        
+        let firstViewController = UIViewController()
+        let secondViewController = UIViewController()
+
+        let pagingViewController = PagingViewController(viewControllers: [
+          firstViewController,
+          secondViewController
+        ])
     }
 
 
