1a2ad2359726ae01a5434debe1d4d64a4fdc581e
邓江洲 <dengjiangzhouReal@gmail.com>
2021-04-12 10:46:14 +0800

    do start

---
+++ boom/boom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  boom
+//
+//  Created by Jz D on 2021/4/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
+    }
+
+
+}
+
